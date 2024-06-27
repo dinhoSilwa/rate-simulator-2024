575e6b08f77192862f00a0dfbfe13f7d765c511d
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-06-26 22:52:01 -0300

    Add ajust

--- src/componentes/GetRate.tsx
+++ src/componentes/GetRate.tsx
@@ -1,5 +1,8 @@
 import React, { FormEvent, useState } from "react";
 import { Irate } from "../types/conpany";
+import { EllipsisVertical } from "lucide-react";
+import clsx from "clsx";
+import toast, { Toaster } from 'react-hot-toast';
 
 const mercadopagorate: Irate[] = [
   { parcela: 1, taxa: 2.99 },
@@ -15,14 +18,18 @@ const mercadopagorate: Irate[] = [
   { parcela: 12, taxa: 12.29 },
 ];
 
-export const GetRate = () => {
-  interface Ioperation {
-    originalvalue: number;
-    valuewhithrate: number;
-    rate: number;
-    portion: number;
-  }
+interface Ioperation {
+  originalvalue: number;
+  valuewhithrate: number;
+  rate: number;
+  portion: number;
+}
 
+export const GetRate = () => {
+
+  const notify = (msg : string) => toast.error(msg)
+
+  const [isRate, setisRate] = useState(false);
   const [inputvalue, setinputvalue] = useState<Ioperation>({
     originalvalue: 0,
     valuewhithrate: 0,
@@ -34,9 +41,13 @@ export const GetRate = () => {
     event.preventDefault();
 
     if (inputvalue.originalvalue > inputvalue.valuewhithrate) {
-      return alert(
-        "O valor original não pode ser maior do que o Valor com A taxa"
-      );
+       notify("O valor do primeiro campo não pode ser menor do que o do segundo")
+       return
+
+    }else if(inputvalue.originalvalue >= 0 && inputvalue.valuewhithrate >= 0){
+      notify("Não pode existir campos vazios")
+      return
+
     }
 
     const rate =
@@ -47,17 +58,101 @@ export const GetRate = () => {
       ...prevState,
       rate,
     }));
+
+    setisRate(!isRate);
   };
+
   return (
     <>
-      <form className="flex flex-col" onSubmit={findRate}>
-        Achar Taxa Aplicada
+
+    <Toaster />
+      <section>
+        {/* {inputvalue.originalvalue} - {inputvalue.valuewhithrate} - {inputvalue.rate} - {inputvalue.portion} */}
+      </section>
+      <header className="flex flex-col bg-blue-600 py-8">
+        <nav className="flex items-center justify-between px-6 h-20">
+          <h2 className="font-semibold text-[18px] text-blue-100">
+            Encontrar Taxa Aplicada
+          </h2>{" "}
+          <span className="active:bg-blue-800 p-4 rounded-full cursor-pointer">
+            <EllipsisVertical className="text-blue-100" />
+          </span>
+        </nav>
+
+        <section
+          className={clsx(
+            "bg-white rounded-lg w-[90%] ml-auto mr-auto px-4 py-8 flex flex-col gap-6",
+            { hidden: !isRate }
+          )}
+        >
+          <header className="flex justify-between pr-6 flex-col">
+            <h3 className="w-full text-[8px] text-slate-500 font-semibold font-[inter] ">
+              valor total com a taxa
+            </h3>
+            <section className="flex justify-between items-center">
+              <div className="flex w-1/2 gap-2">
+                <span className="text-[32px] font-[inter] text-zinc-400">
+                  R$
+                </span>
+                <span className="text-[32px] font-[inter] text-slate-800 font-bold">
+                  {Number(inputvalue.valuewhithrate) || 0}
+                </span>
+              </div>
+
+              <div className="flex border border-green-400 w-[70px] h-8 rounded-2xl items-center justify-center text-green-600 shadow-green-300">
+                {Number(inputvalue.rate || 0)} %
+              </div>
+            </section>
+          </header>
+
+          <section className="flex justify-between">
+            <div className=" border-l-[3px] pl-2 border-red-600 h-8 w-[50%]">
+              <div className="w-full text-[8px] text-slate-500 font-semibold font-[inter]">
+                valor da venda
+              </div>
+
+              <div className="text-[14px] font-bold text-slate-800">
+                {(Number(inputvalue.originalvalue) || 0).toLocaleString(
+                  "pt-BR",
+                  { style: "currency", currency: "BRL" }
+                )}
+              </div>
+            </div>
+
+            <div className=" border-l-[4px] pl-2 border-blue-300 h-8 w-[50%]">
+              <div className="w-full text-[8px] text-slate-500 font-semibold font-[inter]">
+                Parcelamento
+              </div>
+
+              <div className="text-[14px] font-bold text-slate-800">
+                {(
+                  Number(inputvalue.valuewhithrate / inputvalue.portion) || 0
+                ).toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                })}
+              </div>
+            </div>
+          </section>
+        </section>
+      </header>
+
+      <form
+        className="flex flex-col px-4 rounded-lg py-10 font-[inter] gap-4"
+        onSubmit={findRate}
+      >
         <fieldset className="flex flex-col gap-2">
-          <label htmlFor="valorpassado">Digite o valor da venda</label>
+          <label
+            htmlFor="valorpassado"
+            className="font-bold text-zinc-500 text-[14px]"
+          >
+            Digite o valor da venda
+          </label>
           <input
+          
             type="text"
             id="valorpassado"
-            className="bg-zinc-200 px-2 py-2"
+            className="bg-zinc-200 pl-4 py-4 rounded-lg"
             value={Number(inputvalue.originalvalue) || 0}
             onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
               setinputvalue((prevState) => ({
@@ -65,15 +160,19 @@ export const GetRate = () => {
                 originalvalue: parseFloat(e.target.value),
               }));
             }}
+            
           />
         </fieldset>
         <fieldset className="flex flex-col gap-2">
-          <label htmlFor="ValorPassado">
+          <label
+            htmlFor="ValorPassado"
+            className="font-bold text-zinc-500 text-[14px]"
+          >
             Digite o valor total (com a taxa)
           </label>
           <input
             type="text"
-            className="bg-zinc-200 px-2 py-2"
+            className="bg-zinc-200 pl-4 py-4 rounded-lg"
             value={Number(inputvalue.valuewhithrate) || 0}
             onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
               setinputvalue((prevState) => ({
@@ -83,7 +182,9 @@ export const GetRate = () => {
             }
           />
         </fieldset>
-        <fieldset className="flex gap-2 flex-wrap">
+
+        <span>Quantidade de Parcelas:</span>
+        <fieldset className="flex gap-2 flex-wrap items-center mb-6">
           {mercadopagorate.map(({ parcela }, index) => (
             <>
               <label
@@ -111,31 +212,16 @@ export const GetRate = () => {
             </>
           ))}
         </fieldset>
-        <fieldset>
+        <fieldset className="flex items-center justify-center">
           <button
             type="submit"
-            className="bg-green-600 text-white py-2 w-[60%] rounded-md"
+            className="bg-green-600 text-white py-2 w-[80%] rounded-md h-16"
           >
             Encontrar Taxa
           </button>
         </fieldset>
       </form>
 
-      <section className=" px-8">
-        <section className="bg-blue-200 flex flex-col items-center rounded-lg gap-2">
-          <header className="flex justify-center flex-col items-center">
-            <h2>Valor da venda : </h2>{" "}
-            <strong>{inputvalue.originalvalue}</strong>
-          </header>
-          <div className="flex gap-6">
-            <span> Valor Total : {inputvalue.valuewhithrate} </span>
-            <span>
-              {(inputvalue.valuewhithrate / inputvalue.portion).toFixed(2)}
-            </span>
-            <span>Taxa Aplicada : {inputvalue.rate.toFixed(2)}% </span>
-          </div>
-        </section>
-      </section>
     </>
   );
 };
